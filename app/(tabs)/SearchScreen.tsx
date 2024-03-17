63ff1e9dd69e9900a71c8438150d3a8bf7f571aa
Efe Esen <efeesen1390@gmail.com>
2024-03-17 14:47:58 +0300

    search screen essentials

--- app/(tabs)/SearchScreen.tsx
+++ app/(tabs)/SearchScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const SearchScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Search</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search by Book title, Author, ISBN"
+          placeholderTextColor="#888"
+          // search functionality here
+        />
+        <FontAwesome name="search" size={24} color="black" style={styles.searchIcon} />
       </View>
+    </View>
   );
 };
 
@@ -14,6 +24,29 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 40,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  searchIcon: {
+    marginLeft: 10,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 18,
+    color: '#333',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
